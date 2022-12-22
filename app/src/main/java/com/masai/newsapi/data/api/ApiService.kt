ec06efd898ca6554c9a23b6c4bba496e362f622e
Bhavesh Patil <75352379+bhaveshppatil@users.noreply.github.com>
2022-12-22 19:26:09 +0530

    Update ApiService.kt

--- app/src/main/java/com/masai/newsapi/data/api/ApiService.kt
+++ app/src/main/java/com/masai/newsapi/data/api/ApiService.kt
@@ -7,8 +7,8 @@ import retrofit2.http.Query
 
 interface ApiService {
 
-    //https://newsapi.org/v2/everything?q=tesla&from=2021-12-24&to=2021-12-24&apiKey=3f355e979ae645aaa0e437ec430d011d
-    //https://newsapi.org/v2/everything?q=tesla&apiKey=3f355e979ae645aaa0e437ec430d011d
+    //https://newsapi.org/v2/everything?q=tesla&from=2021-12-24&to=2021-12-24&apiKey="API_Key"
+    //https://newsapi.org/v2/everything?q=tesla&apiKey="API_Key"
 
     @GET("v2/everything")
     suspend fun getNewsData(
